9bd132e9e05028d5b285b379f84b43aa5332d31f
wenjinchoi <wenjin.choi@gmail.com>
2013-02-26 23:46:16 +0800

    Add Normal Class Function

--- test_contact_data_deleted.py
+++ test_contact_data_deleted.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 
 import os
-import unittest
 import sqlite3
+import unittest
 
 from contactsTemplates import *
 from sqlite_utily import *
@@ -19,35 +19,48 @@ class ContactsDataDeletedTestCase(unittest.TestCase):
 
 		if not os.path.exists('./src_db'): os.mkdir('./src_db')
 		if not os.path.exists('./rec_db'): os.mkdir('./rec_db')
-
 		self.input_db_file = './src_db/contacts2.db'
 		self.output_db_file = './rec_db/contacts2.db'
 
 		createDB(self.input_db_file, self.schema)
 
-	def insertNormalTestData(self, db_file, testData):
-		with sqlite3.connect(db_file) as conn:
+	def insertTestDataWithSQL(self, insertSQL, testData):
+		with sqlite3.connect(self.input_db_file) as conn:
 			curs = conn.cursor()
-			insertSQL = '''INSERT INTO data(package_id,mimetype_id,raw_contact_id,data1)
-				VALUES (?, ?, ?, ?)'''
 			for n in xrange(len(testData)):
 				curs.execute(insertSQL, testData[n])
 			conn.commit()
 
-	def deleteDataTableWith_ID(self, db_file, id):
-		with sqlite3.connect(db_file) as conn:
+	def insertNormalTestData(self, testData):
+		insertSQL = '''INSERT INTO data(
+			package_id,mimetype_id,raw_contact_id,data1)
+			VALUES (?, ?, ?, ?)'''
+		self.insertTestDataWithSQL(insertSQL, testData)
+
+	def deleteDataTableWith_ID(self, id):
+		with sqlite3.connect(self.input_db_file) as conn:
 			curs = conn.cursor()
 			deleteSQL = '''DELETE FROM data WHERE _id = %d''' % id
 			curs.execute(deleteSQL)
 			conn.commit()
+
+	def parsingDataTableByLoadLibrary(self):
+		pass
+
+	def checkResultWithTestDataNum(self, num):
+		pass
 
 	def testBase(self):
 		testData = (
 			(1, 1, 1, "10086"),
 			(1, 1, 1, "13800138000"))
 
-		self.insertNormalTestData(self.input_db_file, testData)
-		self.deleteDataTableWith_ID(self.input_db_file, 2)
+		deleteRecordID = 2
+
+		self.insertNormalTestData(testData)
+		self.deleteDataTableWith_ID(deleteRecordID)
+		self.parsingDataTableByLoadLibrary()
+		self.checkResultWithTestDataNum(deleteRecordID-1)
 
 
 if __name__ == '__main__':
